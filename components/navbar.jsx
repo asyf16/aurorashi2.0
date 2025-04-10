e6142feaacec7f304564495040454fb688c00787
asyf16 <aurorashi16@gmail.com>
2025-04-10 01:40:39 -0400

    Add hamburger menu

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,8 +1,9 @@
 'use client';
-import { Volume2, VolumeOff } from 'lucide-react';
+import { Volume2, VolumeOff, Menu, X } from 'lucide-react';
 import { useState } from 'react';
 
 export default function Navbar() {
+    const [openMenu, setOpenMenu] = useState(false);
     const [volume, setVolume] = useState(false);
     return (
         <div className="z-[999] w-screen h-[60px] flex flex-row justify-center items-center fixed top-0 left-0 backdrop-blur-md text-[#e6e6e6] bg-[#14102ccc] shadow-lg">
@@ -27,16 +28,44 @@ export default function Navbar() {
             </div>
             <a
                 href="#home"
-                className="hover:text-[#d1c8ffff] sm:left-18 left-6 absolute text-[17px] font-heading hover:italic"
+                className="hover:text-[#d1c8ffff] sm:left-18 left-8 absolute text-[17px] font-heading hover:italic"
             >
                 Aurora Shi
             </a>
-            <div
-                className="hover:text-[#d1c8ffff] sm:right-18 right-6 absolute border border-[#e6e6e6] hover:border-[#d1c8ffff] rounded-lg p-1 cursor-pointer"
-                onClick={() => setVolume(!volume)}
-            >
-                {volume ? <Volume2 className="w-4 h-4" /> : <VolumeOff className="w-4 h-4" />}
+            <div className="flex flex-row gap-4 sm:right-18 right-8 absolute">
+                {openMenu ? (
+                    <X className="sm:hidden flex w-6 h-6" onClick={() => setOpenMenu(!openMenu)} />
+                ) : (
+                    <Menu className="sm:hidden flex w-6 h-6" onClick={() => setOpenMenu(!openMenu)} />
+                )}
+                <div
+                    className="hover:text-[#d1c8ffff] border border-[#e6e6e6] hover:border-[#d1c8ffff] rounded-lg p-1 cursor-pointer"
+                    onClick={() => setVolume(!volume)}
+                >
+                    {volume ? <Volume2 className="w-4 h-4" /> : <VolumeOff className="w-4 h-4" />}
+                </div>
             </div>
+                {openMenu && (
+                    <div className="flex flex-col border border-[#292442] sm:hidden fixed top-[70px] right-[10px] rounded-lg backdrop-blur-md text-[#e6e6e6] bg-[#1a1536cc] shadow-lg px-6 py-2 gap-2 text-lg">
+                        <a href="#about">
+                            <div className=" hover:text-[#d1c8ffff] hover:italic cursor-pointer"><span className="font-serif italic font-bold">1.</span> About</div>
+                        </a>
+                        <a href="#experience">
+                            <div className="hover:text-[#d1c8ffff] hover:italic cursor-pointer"><span className="font-serif italic font-bold">2.</span> Experience</div>
+                        </a>
+                        <a href="#projects">
+                            <div className="hover:text-[#d1c8ffff] hover:italic cursor-pointer"><span className="font-serif italic font-bold">3.</span> Projects</div>
+                        </a>
+
+                        <a href="#portfolio">
+                            <div className="hover:text-[#d1c8ffff] hover:italic cursor-pointer"><span className="font-serif italic font-bold">4.</span> Portfolio</div>
+                        </a>
+
+                        <a href="#about">
+                            <div className="hover:text-[#d1c8ffff] hover:italic cursor-pointer"><span className="font-serif italic font-bold">5.</span> Click</div>
+                        </a>
+                    </div>
+                )}
         </div>
     );
 }
