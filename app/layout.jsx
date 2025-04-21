1584f0a680dc092cca36018ed1872b0f82655c2d
asyf16 <aurorashi16@gmail.com>
2025-04-21 16:00:37 -0700

    ga tracking

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 import ContextProvider from 'components/contextProvider';
+import Head from 'next/head';
 
 export const metadata = {
     title: {
@@ -12,6 +13,18 @@ export default function RootLayout({ children }) {
         <html lang="en">
             <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q74TEFFYS2"></script>
+                <Head>
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q74TEFFYS2"></script>
+                    <script>
+                        {`
+                          window.dataLayer = window.dataLayer || [];
+                          function gtag(){dataLayer.push(arguments);}
+                          gtag('js', new Date());
+                          gtag('config', 'G-Q74TEFFYS2');
+                        `}
+                    </script>
+                </Head>
             </head>
             <body className="antialiased bg-[#04020e]">
                 <ContextProvider>
