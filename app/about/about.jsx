3ba025be9c9a34de42663d57ebf8a2a68e4b30d9
asyf16 <aurorashi16@gmail.com>
2025-04-24 13:10:53 -0700

    add se webring

--- app/about/about.jsx
+++ app/about/about.jsx
@@ -35,7 +35,8 @@ export default function About() {
                                     className="rounded-lg"
                                 />
                             </div>
-                        </span>{' '}, gym-goer, and{' '}
+                        </span>{' '}
+                        , gym-goer, and{' '}
                         <span className="font-serif italic text-[#d1c8ffff] font-bold group relative">
                             fisher(wo)man
                             <div className="left-[20px] top-[80px] hidden absolute group-hover:block transform scale-220 transition-all duration-300 z-[999]">
@@ -47,7 +48,8 @@ export default function About() {
                                     className="rounded-lg"
                                 />
                             </div>
-                        </span>{' '}. I also love{' '}
+                        </span>{' '}
+                        . I also love{' '}
                         <span className="font-serif italic text-[#d1c8ffff] font-bold group relative">
                             baking
                             <div className="left-[20px] top-[105px] hidden absolute group-hover:block transform scale-550 transition-all duration-300 z-[999]">
@@ -70,7 +72,7 @@ export default function About() {
                     </div>
                     <div className="sm:text-lg text-sm">Thanks for checking out my projects and portfolio :)</div>
                     <div>
-                        <div className="flex gap-2 mt-4 text-[#d1c8ffff]">
+                        <div className="flex gap-2 mt-4 text-[#d1c8ffff] items-center">
                             <a href="https://www.instagram.com/aurorashi16/" target="_blank" rel="noopener noreferrer">
                                 <Instagram className="h-5 w-5 text-white hover:text-[#d1c8ffff] transition" />
                             </a>
@@ -85,6 +87,23 @@ export default function About() {
                             </a>
                             <a href="https://beliapp.co/app/asyf16" target="_blank" rel="noopener noreferrer">
                                 <Beef className="h-5 w-5 text-white hover:text-[#d1c8ffff] transition" />
+                            </a>
+                            <a href="https://se-webring.xyz/" target="_blank" rel="noopener noreferrer">
+                                <svg
+                                    width="25"
+                                    height="25"
+                                    viewBox="0 0 960 960"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="text-white hover:text-[#d1c8ffff]"
+                                    fill="currentColor"
+                                >
+                                    <rect width="960" height="960" fill="none" />
+                                    <path
+                                        fillRule="evenodd"
+                                        clipRule="evenodd"
+                                        d="M390.499 272.146V687.477C491.737 672.742 569.501 585.38 569.501 479.811C569.501 374.243 491.737 286.881 390.499 272.146ZM367.396 749.527C364.991 749.591 362.577 749.623 360.157 749.623C211.506 749.623 91 628.824 91 479.811C91 330.799 211.506 210 360.157 210C362.577 210 364.991 210.032 367.396 210.096C368.31 210.032 369.233 210 370.163 210H869V749.623H370.163C369.233 749.623 368.31 749.591 367.396 749.527ZM330.687 687.602V272.021C229.023 286.379 150.813 373.94 150.813 479.811C150.813 585.683 229.023 673.244 330.687 687.602ZM529.346 269.958H809.187V689.665H529.346C590.325 640.201 629.313 564.57 629.313 479.811C629.313 395.053 590.325 319.422 529.346 269.958Z"
+                                    />
+                                </svg>
                             </a>
                         </div>
                     </div>
